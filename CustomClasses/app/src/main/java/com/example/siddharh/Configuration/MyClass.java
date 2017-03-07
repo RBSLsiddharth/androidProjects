af777972c68b85d7c3cdac828c719706e3101ec0
RBSLsiddharth <siddharth@readybytes.in>
2017-03-07 10:09:57 +0530

    applied handlers in Loginclass where also created a threadgeneration function in which generates a thread over a trigger, on anshul's code

--- CustomClasses/app/src/main/java/com/example/siddharh/Configuration/MyClass.java
+++
@@ -1,9 +0,0 @@
-package com.example.siddharh.Configuration;
-
-/**
- * Created by siddharh on 22/2/17.
- */
-
-public interface MyClass {
-
-}
---
+++ CustomClasses/app/src/main/java/com/example/siddharh/Configuration/MyThread.java
@@ -0,0 +1,8 @@
+package com.example.siddharh.Configuration;
+
+/**
+ * Created by siddharh on 25/2/17.
+ */
+
+public class MyThread {
+}
